29bd86d9fbb620e882bdd5d28c2a2a138dbbd89f
samarpit-sachan <samarpitsachan091@gmail.com>
2023-07-20 00:45:55 +0530

    navbar login automodal resetpassword signup pages boilerplate code done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-    <Head>
+	return (
+		<RecoilRoot>
+			<Head>
 				<title>LeetClone</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.png' />
@@ -14,7 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
 					content='Web application that contains leetcode problems and video solutions'
 				/>
 			</Head>
-  <Component {...pageProps} />
-  </>
-  )
+			<Component {...pageProps} />
+		</RecoilRoot>
+	)
 }
